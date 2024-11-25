3b66119eb31dfe93503f7cf26ca97ac441365b0f
Pawan Kumar <pawankumar00@live.com>
2024-11-25 00:30:56 -0500

    Made Changes to User MVC

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,26 +1,28 @@
 const User = require("../models/User");
 const { validationResult } = require('express-validator');
-const jwt = require('jsonwebtoken');
-const cloudinary = require('cloudinary');
-const generateToken = require('../utils/jwtToken');
+var jwt = require('jsonwebtoken');
 const catchAsyncErrors = require('../Middleware/catchAsyncErrors');
+const cloudinary = require('cloudinary')
+const generateToken = require('../utils/jwtToken');
 
-// SignUp - Create new user
 exports.signup = catchAsyncErrors(async (req, res) => {
-    const errors = validationResult(req);
+    const errors = validationResult(req)
+
     if (!errors.isEmpty()) {
-        return res.status(400).json({ error: errors.array()[0].msg });
+        return res.status(400).json({
+            error: errors.array()[0].msg
+        })
     }
 
     const { name, phonenumber, deliveryaddress, email, password, avatar } = req.body;
 
-    if (avatar !== '') {
-        const result = await cloudinary.v2.uploader.upload(avatar, {
+  
+    if (req.body.avatar !== '') {
+        const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
             folder: 'avatars',
             width: 800,
             crop: "scale"
-        });
-
+        })
         const user = new User({
             name,
             phonenumber,
@@ -31,138 +33,195 @@ exports.signup = catchAsyncErrors(async (req, res) => {
                 public_id: result.public_id,
                 url: result.secure_url
             }
-        });
-
-        user.save((err, newUser) => {
+        })
+        user.save((err, newuser) => {
             if (err) {
-                return res.status(400).json({ error: "Error!" });
+                return res.status(400).json({
+                    error: "Error!"
+                })
             }
-            generateToken(newUser, 200, res);
-        });
 
-    } else {
+            generateToken(newuser, 200, res)
+
+           
+        })
+
+    }
+    else {
         const user = new User({
             name,
             phonenumber,
-            email,
             deliveryaddress,
+            email,
             password
-        });
-
-        user.save((err, newUser) => {
+        })
+        user.save((err, newuser) => {
             if (err) {
-                return res.status(400).json({ error: "Error!" });
+                return res.status(400).json({
+                    error: "Error!"
+                })
             }
-            generateToken(newUser, 200, res);
-        });
+            generateToken(newuser, 200, res)
+            
+        })
     }
-});
+})
 
-// SignIn - User login
+
 exports.signin = (req, res) => {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
     User.findOne({ email }, (err, user) => {
         if (err || !user) {
-            return res.status(400).json({ error: "Email Address not found" });
+            return res.status(400).json({
+                error: "Email Address not found"
+            })
         }
         if (user.account_status === "inactive") {
-            return res.status(400).json({ error: "This account has been deleted" });
+            return res.status(400).json({
+                error: "This account has been deleted"
+            })
         }
-
+        // check if user's email and password are correct       
         if (!user.authenticate(password)) {
-            return res.status(400).json({ error: "Email Address or Password is incorrect" });
+            return res.status(400).json({
+                error: "Email Address or Password is incorrect"
+            })
         }
 
-        generateToken(user, 200, res);
-    });
-};
+        generateToken(user, 200, res)
 
-// SignOut - User logout
+       
+    })
+}
+
 exports.signout = (req, res) => {
+    // res.clearCookie("token")
     res.cookie('token', null, {
         expires: new Date(Date.now()),
         httpOnly: true
-    });
+    })
+    //req.session = null
+    return res.json({
+        success: true,
+        message: "User signed out successfully."
+    })
+}
 
-    return res.json({ success: true, message: "User signed out successfully." });
-};
-
-// GetProfile - View user profile
-exports.getprofile = catchAsyncErrors(async (req, res) => {
-    const user = await User.findById(req.user._id);
-
-    if (user) {
-        const { _id, name, email, phonenumber, deliveryaddress, role, createdAt, avatar } = user;
-        return res.status(200).json({
-            message: "success",
-            user: { _id, name, email, phonenumber, deliveryaddress, role, createdAt, avatar }
-        });
-    } else {
-        return res.status(404).json({ error: "User not found" });
+// View your Profile
+exports.getprofile = catchAsyncErrors(async (req, res, next) => {
+    
+    if (req.user) {
+        // console.log("hello get profile")
+        User.findOne({ _id: req.user._id }, (err, user) => {
+            const { _id, name, email, phonenumber, deliveryaddress, role, createdAt, avatar } = user
+            return res.status(200).json({
+                message: "success",
+                user: {
+                    _id,
+                    name,
+                    email,
+                    phonenumber,
+                    deliveryaddress,
+                    role,
+                    createdAt,
+                    avatar
+                }
+            })
+        })
     }
-});
+})
 
-// UpdateProfile - Update user profile
+// Update your profile  
 exports.updateProfile = catchAsyncErrors(async (req, res) => {
     const newUserData = {
         name: req.body.name,
         email: req.body.email,
         phonenumber: req.body.phonenumber,
         deliveryaddress: req.body.deliveryaddress
-    };
+    }
 
-    const user = await User.findById(req.user._id);
 
-    if (req.body.avatar) {
-        if (user.avatar.public_id) {
-            await cloudinary.v2.uploader.destroy(user.avatar.public_id);
+    //console.log(req.body)
+    console.log('*****************')
+
+    
+    // Update avatar
+    //if (req.body.avatar !== '') {
+
+
+    const user = await User.findById(req.user._id)
+    console.log(user)
+    console.log('-------------------')
+
+
+
+if (req.body.avatar) {
+
+        var image_id = user.avatar.public_id;
+        if (image_id) {
+            console.log("Before: " + user.avatar.public_id)
+            console.log("After:" + user.avatar.public_id)
+            const resp = await cloudinary.v2.uploader.destroy(image_id);
         }
 
         const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
             folder: 'avatars',
             width: 800,
             crop: "scale"
-        });
+        })
+        console.log("here " + result.public_id)
 
         newUserData.avatar = {
             public_id: result.public_id,
             url: result.secure_url
-        };
+        }
     }
 
-    const updatedUser = await User.findByIdAndUpdate(req.user._id, newUserData, {
+
+    user1 = await User.findByIdAndUpdate(req.user._id, newUserData, {
         new: true,
         runValidators: true,
         useFindAndModify: false
-    });
+    })
 
-    return res.status(200).json({ success: true, updatedUser });
-});
+    console.log(user1)
 
-// UpdatePassword - Change password
-exports.updatePassword = catchAsyncErrors(async (req, res) => {
-    const { oldPassword, password } = req.body;
+    res.status(200).json({
+        success: true
+    })
 
-    const user = await User.findById(req.user._id).select('+encrypted_password');
-    if (!user.authenticate(oldPassword)) {
-        return res.status(400).json({ error: 'Old password is incorrect' });
+})
+
+// Update / Change password   =>  /api/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user._id).select('+password');
+
+    // Checks previous password of user
+    const isMatched = await user.authenticate(req.body.oldPassword)
+    if (!isMatched) {
+        return res.status(404).json({
+            error: 'Old password is incorrect'
+        })
     }
 
-    user.password = password;
+    user.password = req.body.password;
     await user.save();
 
-    generateToken(user, 200, res);
-});
+    generateToken(user, 200, res)
+})
 
-// DeleteAccount - Delete your account
+// Delete your account - For any user
 exports.deleteYourAccount = catchAsyncErrors(async (req, res) => {
     const user = await User.findById(req.user._id);
-    if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-    }
 
-    await User.findByIdAndUpdate(req.user._id, { account_status: "inactive" }, {
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: 'User not found'
+        })
+    }
+    updateduser = await User.findByIdAndUpdate(req.user._id, { account_status: "inactive" }, {
         new: true,
         runValidators: true,
         useFindAndModify: false
@@ -171,61 +230,155 @@ exports.deleteYourAccount = catchAsyncErrors(async (req, res) => {
     res.cookie('token', null, {
         expires: new Date(Date.now()),
         httpOnly: true
-    });
+    })
 
-    res.status(200).json({ success: true, message: "Account Deleted Successfully" });
-});
+    res.status(200).json({
+        success: true,
+        message: "Account Deleted Successfully"
+        //updateduser
+    })
+})
 
-// UpdateUser - Admin can update user profile
+
+// Update user profile -- For Admins
 exports.updateUser = (req, res) => {
+
     User.findOneAndUpdate(
-        { _id: req.params.id, role: "user" },
-        req.body,
-        { new: true, runValidators: true, useFindAndModify: false }
+        { $and: [{ _id: { $eq: req.params.id } }, { role: "user" }] }, // Find one id equals to id in params
+        req.body, // data to be updated
+        {
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+        }
     )
         .then(user => {
             if (!user) {
-                return res.status(200).json({ message: "You cannot update an admin" });
+                return res.status(200).json({
+                    message: "You cannot update an admin"
+                })
             }
-            return res.status(200).json({ success: true, user });
+            return res.status(200).json({
+                success: true,
+                user
+            })
         })
-        .catch(err => res.status(500).json({ error: err.message }));
-};
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+}
 
-// GetAllUsers - Admin can get all users
+// get all users -- For Admins
 exports.getAllUsers = (req, res) => {
     User.find()
-        .then(users => res.status(200).json({ success: true, users }))
-        .catch(err => res.status(500).json({ error: err.message }));
-};
+        .then(users => {
+            // let type = typeof users;
+            // console.log(type)
+            res.status(200).json({
+                success: true,
+                users
+            })
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+}
 
-// GetUserDetails - Admin can get details of any user
+// Get user details -- For admins
 exports.getUserDetails = (req, res) => {
+
     User.findById(req.params.id)
         .then(user => {
             if (!user) {
-                return res.status(404).json({ message: "User not found" });
+                return res.status(404).json({
+                    message: "User not found"
+                });
             }
-            return res.status(200).json({ success: true, user });
+            return res.status(200).json({
+                success: true,
+                user
+            })
         })
-        .catch(err => res.status(500).json({ error: err.message }));
-};
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+}
 
-// DeactivateUser - Admin can deactivate any user
+
+// Deactivate a user -- For Admins
 exports.deactivateUser = catchAsyncErrors(async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-        return res.status(404).json({ success: false, message: 'User not found' });
-    }
 
-    if (user.role === "user") {
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, { account_status: "inactive" }, {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: 'User not found'
+        })
+    }
+    if (user.role == "user") {
+        const updateduser = await User.findByIdAndUpdate(req.params.id, { account_status: "inactive" }, {
             new: true,
             runValidators: true,
             useFindAndModify: false
         });
-        return res.status(200).json({ success: true, user: updatedUser });
-    } else {
-        return res.status(200).json({ message: "You cannot deactivate an admin" });
+
+        res.status(200).json({
+            success: true,
+            user: updateduser
+        })
     }
-});
+    else {
+        return res.status(200).json({
+            message: "You cannot update an admin"
+        })
+    }
+})
+
+
+// Update any user's profile 
+exports.updateAnyUser = (req, res) => {
+    const { name, phonenumber, email, role } = req.body;
+    User.findOneAndUpdate(
+        { $and: [{ _id: { $eq: req.params.id } }, { $or: [{ role: "user" }, { role: "admin" }] }] }, // Find one id equals to id in params
+        { name, phonenumber, email, role }, // data to be updated
+        { new: true, useFindAndModify: false } // to mongoose returns the updated document
+    )
+        .then(user => {
+            if (!user) {
+                return res.status(200).json({
+                    message: "You cannot update a superadmin"
+                })
+            }
+            return res.status(200).json({
+                success: true,
+                user
+            })
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+}
+
+// Deactivate any user
+exports.deactivateAnyUser = (req, res) => {
+    User.findOneAndUpdate(
+        { $and: [{ _id: { $eq: req.params.id } }, { $or: [{ role: "user" }, { role: "admin" }] }] }, // Find one id equals to id in params and role equal to user
+        { account_status: "inactive" }, // data to be updated
+        { new: true, useFindAndModify: false } // to mongoose returns the updated document
+    )
+        .then(user => {
+            if (!user) {
+                return res.status(200).json({
+                    message: "You cannot update a superadmin"
+                })
+            }
+            res.status(200).json({
+                success: true,
+                user
+            })
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+}
--- BackEnd/routes/userRoutes.js
+++ BackEnd/routes/userRoutes.js
@@ -1,17 +1,52 @@
 // routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
+const { signup, signin, signout, getprofile, updateUser, getAllUsers, getUserDetails, updateProfile, updatePassword, deactivateUser, updateAnyUser, deactivateAnyUser, deleteYourAccount} = require("../controllers/userController")
+const {verifyToken, authorizeRoles} = require("../Middleware/authMiddleware");
+const {check} = require('express-validator')
 const userController = require('../controllers/userController');
 
 // Define the routes and connect them to the controller functions
-router.post('/users',function(req, res){
-    userController.Create
-  });
-router.get('/users', function(req, res){
-    userController.getUser
-  });
-router.get('/users/:id',function(req, res){
-    userController.getUserById
-  });
+router.post('/signup', [
+     check("name", "Name should be atleast 3 characters").isLength({min: 3}),
+     check("phonenumber", "Phone Number should be exactly 11 digits").isLength({min: 11, max:11}),
+     check("email", "Email Address should be valid").isEmail(),
+     check("password", "Password should be atleast 8 characters").isLength({min: 8}),
+ ], signup)
 
-module.exports = router;
+//  Login Route for all users
+router.post("/login", signin)
+
+//superadmin routes
+//  update any particular user
+// router.put("/superadmin/user/:id", verifyToken, authorizeRoles("superadmin"), updateAnyUser)
+// deactivate a user
+// router.put("/superadmin/deactivate/user/:id", verifyToken, authorizeRoles("superadmin"), deactivateAnyUser)
+
+
+// admin routes
+//  update a particular user
+router.put("/admin/user/:id", verifyToken, authorizeRoles("admin"), updateUser)
+// deactivate a user
+router.put("/admin/deactivate/user/:id", verifyToken, authorizeRoles("admin"), deactivateUser)
+// view a particular user
+router.get("/admin/user/:id", verifyToken, authorizeRoles("admin"), getUserDetails)
+// get all user
+router.get("/admin/allusers", verifyToken, authorizeRoles("admin"), getAllUsers)
+
+
+// get currently logged in user profile
+router.get('/profile', verifyToken, getprofile)
+
+// update your profile
+router.put('/profile/update', verifyToken, updateProfile)
+
+// update your password
+router.put('/profile/update/password', verifyToken, updatePassword)
+
+// delete your user
+router.put("/myaccount/delete", verifyToken, deleteYourAccount)
+
+router.get('/logout', signout)
+
+module.exports = router
